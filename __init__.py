22c8ffa6bd321e605e35af52ee2d4a00cd5fa9c5
Alexander Nilsson <alenilsson7@gmail.com>
2025-07-14 12:22:25 +0200

    removed some files

--- __init__.py
+++ __init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-# Thanks to Damien Elmes, this plugin is loosely based on his original Plugin and borrows slightly from his project
-# Also thanks to the creators of the Japanese Pronunciation/Pitch Accent, Japanese Pitch Accent Notes, and pitch accent note button  which I also borrowed marginally from
-#
 
-from . import  main, ffmpegInstaller, miUpdater, miflix, checkForThirtyTwo
+from . import  main, ffmpegInstaller, miUpdater, miflix
